9cef7f20fdc97570642b5261922f72a2c95169bc
Patrick Dockhorn <patrick@apptastic.com.au>
2012-05-22 11:28:12 +1000

    initial ios files

---
+++ src/GeoHash-iOS/GNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GNAppDelegate.h
+//  GeoHash-iOS
+//
+//  Created by Patrick Dockhorn on 22/05/12.
+//  Copyright (c) 2012 KJEW Investments Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
